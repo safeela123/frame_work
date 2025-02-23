9ecba2a4c8d8135cda7fcb05aa2523ad2620d064
safeela123 <safeelansrin42484@gmail.com>
2025-02-24 00:55:41 +0530

    project

--- BUILD_UI/APP/models.py
+++ BUILD_UI/APP/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
 class ui(models.Model):
     text=models.TextField()
+    date = models.DateField(auto_now=True)
